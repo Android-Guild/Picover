0e22d7015acc483853c043b62c19b2d761ec6559
Konrad <kmaslowiec@gmail.com>
2023-01-07 14:39:04 +0100

    #1 libraries updated, BOM for compose added, theme for dark mode fixed

--- app/src/main/java/com/example/android/picover/MainActivity.kt
+++ app/src/main/java/com/example/android/picover/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
             PicoverTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Greeting("Picover")
                 }
             }
         }
