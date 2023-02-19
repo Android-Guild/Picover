b8cf396842bc5cbe0ee6dbe87c4efd5cb72fa8c3
Borys Chajdas <b.chajdas@intive.com>
2023-02-19 19:56:49 +0100

    #24 Basic camera usage

--- app/src/main/kotlin/com/intive/picover/camera/view/CameraScreen.kt
+++ app/src/main/kotlin/com/intive/picover/camera/view/CameraScreen.kt
@@ -1,23 +1,101 @@
 package com.intive.picover.camera.view
 
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.intive.picover.R
 import com.intive.picover.camera.viewmodel.CameraViewModel
+import com.intive.picover.common.loader.PicoverLoader
+import com.skydoves.landscapist.ImageOptions
+import com.skydoves.landscapist.coil.CoilImage
 
 @Composable
-fun CameraScreen(cameraViewModel: CameraViewModel) {
+fun CameraScreen(viewModel: CameraViewModel) {
+	LocalLifecycleOwner.current.lifecycle.addObserver(viewModel)
+	val takenImageUri by viewModel.takenImageUri
+	val isImageTaken by viewModel.isImageTaken
+	val cameraLauncher = rememberLauncherForActivityResult(ActivityResultContracts.TakePicture()) {
+		viewModel.onImageTaken(wasSaved = it)
+	}
+	Screen(takenImageUri, isImageTaken) { cameraLauncher.launch(takenImageUri) }
+}
+
+@Composable
+private fun Screen(
+	imageUri: Uri?,
+	isImageTaken: Boolean,
+	takeImage: () -> Unit,
+) {
 	Box(
 		modifier = Modifier.fillMaxSize(),
+		contentAlignment = Alignment.Center,
 	) {
-		Text(
-			text = stringResource(id = R.string.Camera),
-			modifier = Modifier.align(Alignment.Center),
-		)
+		if (imageUri == null) {
+			PicoverLoader()
+		} else {
+			Content(imageUri, isImageTaken, takeImage)
+		}
 	}
+}
+
+@Composable
+private fun Content(
+	imageUri: Uri,
+	isImageTaken: Boolean,
+	takeImage: () -> Unit,
+) {
+	if (isImageTaken) {
+		TakenImage(imageUri)
+	} else {
+		TakePictureButton(onClick = takeImage)
+	}
+}
+
+@Composable
+private fun TakenImage(imageUri: Uri) {
+	CoilImage(
+		imageModel = { imageUri },
+		imageOptions = ImageOptions(
+			contentScale = ContentScale.Crop,
+			alignment = Alignment.Center,
+		),
+		previewPlaceholder = R.drawable.ic_launcher_background,
+	)
+}
+
+@Composable
+private fun TakePictureButton(onClick: () -> Unit) {
+	Button(onClick = onClick) {
+		Text(stringResource(R.string.TakePictureButton))
+	}
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun LoadingPreview() {
+	Screen(imageUri = null, isImageTaken = false) {}
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ImageNotTakenPreview() {
+	Screen(imageUri = Uri.parse(""), isImageTaken = false) {}
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ImageTakenPreview() {
+	Screen(imageUri = Uri.parse(""), isImageTaken = true) {}
 }
--- app/src/main/kotlin/com/intive/picover/camera/viewmodel/CameraViewModel.kt
+++ app/src/main/kotlin/com/intive/picover/camera/viewmodel/CameraViewModel.kt
@@ -1,8 +1,28 @@
 package com.intive.picover.camera.viewmodel
 
-import androidx.lifecycle.ViewModel
+import android.net.Uri
+import androidx.compose.runtime.mutableStateOf
+import com.intive.picover.camera.repository.ImageFileRepository
+import com.intive.picover.common.viewmodel.LifecycleViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+// TODO-BCH Should be unit tested after Kotest setup
 @HiltViewModel
-class CameraViewModel @Inject constructor() : ViewModel()
+class CameraViewModel @Inject constructor(
+	private val imageFileRepository: ImageFileRepository,
+) : LifecycleViewModel() {
+
+	val takenImageUri = mutableStateOf<Uri?>(null)
+	val isImageTaken = mutableStateOf(false)
+
+	override fun onFirstCreate() {
+		takenImageUri.value = imageFileRepository.createTempFileAndGetUri()
+	}
+
+	fun onImageTaken(wasSaved: Boolean) {
+		if (wasSaved) {
+			isImageTaken.value = true
+		}
+	}
+}
