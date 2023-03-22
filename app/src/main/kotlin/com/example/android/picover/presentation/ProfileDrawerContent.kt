5d2b95fce5390e79bbb53cd25414b745e9db1809
Marcin Wiśniewski <marcin.wisniewski@intive.com>
2023-03-22 22:52:49 +0100

    #21 Add oss licenses plugin

--- app/src/main/kotlin/com/example/android/picover/presentation/ProfileDrawerContent.kt
+++ app/src/main/kotlin/com/example/android/picover/presentation/ProfileDrawerContent.kt
@@ -1,14 +1,19 @@
 package com.example.android.picover.presentation
 
+import android.content.Intent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalDrawerSheet
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.core.content.ContextCompat.startActivity
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import com.intive.picover.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -18,9 +23,13 @@ fun ProfileDrawerContent() {
 		Box(
 			modifier = Modifier.fillMaxSize(),
 		) {
-			Text(
-				text = stringResource(R.string.Profile),
+			val context = LocalContext.current
+			ClickableText(
+				text = AnnotatedString(stringResource(R.string.Profile)),
 				modifier = Modifier.align(Alignment.Center),
+				onClick = {
+					startActivity(context, Intent(context, OssLicensesMenuActivity::class.java), null)
+				},
 			)
 		}
 	}
