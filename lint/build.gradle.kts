914d30e0af737631a96e65760fb576ef9597b934
Marek Macko <marek.macko@intive.com>
2023-04-26 16:15:19 +0200

    #23 Cleanup lint rules

--- lint/build.gradle.kts
+++ lint/build.gradle.kts
@@ -5,9 +5,7 @@ plugins {
 dependencies {
 	val lintVersion = "30.4.2"
 	compileOnly("com.android.tools.lint:lint-api:$lintVersion")
-	compileOnly("com.android.tools.lint:lint-checks:$lintVersion")
-	testImplementation(platform("io.kotest:kotest-bom:5.5.5"))
+	testImplementation("io.kotest:kotest-runner-junit5:5.6.1")
 	testImplementation("com.android.tools.lint:lint:$lintVersion")
 	testImplementation("com.android.tools.lint:lint-tests:$lintVersion")
-	testImplementation("io.kotest:kotest-runner-junit5")
 }
