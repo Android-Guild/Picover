a98471fa6018acdf21844337bc3e98de196f9ac2
Marek Macko <marek.macko@intive.com>
2023-04-27 12:29:57 +0200

    #23 Update lint

--- lint/build.gradle.kts
+++ lint/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 dependencies {
-	val lintVersion = "30.4.2"
+	val lintVersion = "31.0.1"
 	compileOnly("com.android.tools.lint:lint-api:$lintVersion")
 	testImplementation("io.kotest:kotest-runner-junit5:5.6.1")
 	testImplementation("com.android.tools.lint:lint:$lintVersion")
