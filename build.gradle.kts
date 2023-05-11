1250388f1c5b3002d94e5f551bea0226c2e8a569
Borys Chajdas <b.chajdas@intive.com>
2023-05-11 16:51:40 +0200

    #65 Update Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
 }
 
 plugins {
-	id("com.android.application") version "7.4.2" apply false
+	id("com.android.application") version "8.0.1" apply false
 	id("org.jetbrains.kotlin.android") version "1.8.0" apply false
 	id("com.google.gms.google-services") version "4.3.15" apply false
 	id("com.google.dagger.hilt.android") version "2.44.2" apply false
