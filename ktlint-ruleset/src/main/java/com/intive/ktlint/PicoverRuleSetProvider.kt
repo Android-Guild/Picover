5f57a3cd9ef82cc4c5859dae653c396399cc2218
Marek Macko <marek.macko@intive.com>
2023-05-11 16:29:04 +0200

    #86 Update ktlint to 0.49

--- ktlint-ruleset/src/main/java/com/intive/ktlint/PicoverRuleSetProvider.kt
+++ ktlint-ruleset/src/main/java/com/intive/ktlint/PicoverRuleSetProvider.kt
@@ -1,18 +1,10 @@
 package com.intive.ktlint
 
-import com.pinterest.ktlint.core.RuleProvider
-import com.pinterest.ktlint.core.RuleSetProviderV2
+import com.pinterest.ktlint.cli.ruleset.core.api.RuleSetProviderV3
+import com.pinterest.ktlint.rule.engine.core.api.RuleProvider
+import com.pinterest.ktlint.rule.engine.core.api.RuleSetId
 
-class PicoverRuleSetProvider : RuleSetProviderV2(
-	id = "picover-rules",
-	about = About(
-		maintainer = "Android Guild - Szczecin",
-		description = "Custom ktlint rules used in Picover app",
-		license = null,
-		repositoryUrl = null,
-		issueTrackerUrl = null
-	)
-) {
+class PicoverRuleSetProvider : RuleSetProviderV3(RuleSetId("picover-rules")) {
 
 	override fun getRuleProviders() = setOf<RuleProvider>()
 }
