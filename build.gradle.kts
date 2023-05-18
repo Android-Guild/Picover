0a1faead1054bc8300838e7f32a24ea8a4102b6d
Marek Macko <marek.macko@intive.com>
2023-05-18 15:39:05 +0200

    #11 Setup toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+@file:Suppress("DSL_SCOPE_VIOLATION")
+
 buildscript {
 	dependencies {
 		classpath("com.google.android.gms:oss-licenses-plugin:0.10.4")
@@ -5,10 +7,10 @@ buildscript {
 }
 
 plugins {
-	id("com.android.application") version "8.0.2" apply false
-	id("org.jetbrains.kotlin.android") version "1.8.21" apply false
-	id("com.google.gms.google-services") version "4.3.15" apply false
-	id("com.google.dagger.hilt.android") version "2.44.2" apply false
+	alias(libs.plugins.android.application) apply false
+	alias(libs.plugins.kotlin.android) apply false
+	alias(libs.plugins.google.services) apply false
+	alias(libs.plugins.hilt.android) apply false
 }
 
 subprojects {
