d1e34776ef9d2b1e53fec48d6b2d0e33d8b6d2f3
Borys Chajdas <b.chajdas@intive.com>
2023-05-23 11:24:38 +0200

    #65 Auth repository implementation

---
+++ app/src/main/kotlin/com/intive/picover/auth/di/AuthModule.kt
@@ -0,0 +1,17 @@
+package com.intive.picover.auth.di
+
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AuthModule {
+
+	@Provides
+	fun provideFirebaseAuth() =
+		Firebase.auth
+}
