f159a23d5812b3fc941f58cd95eae0ac8ecb8d2e
nataniel.antosik <nataniel.antosik@intive.com>
2023-05-23 18:40:59 +0200

    #64 Fix checking commit message

--- .github/scripts/commit/checkCommitMessage.sh
+++ .github/scripts/commit/checkCommitMessage.sh
@@ -4,6 +4,6 @@ message=$(git log -1 --pretty=format:"%s" | tr -dc '[:print:]\n')
 if ! [[ "$message" =~ ^#[0-9]+[[:space:]][^[:cntrl:]]+[[:space:]]*$ ]]
 then
   echo "::error::Message is invalid — $message"
-  echo "::error::Commit message should look like - #\$issue_number \$description"
+  echo "::error::Commit message should look like - #\$issue_number \$description "
   exit 1
 fi
