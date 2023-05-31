3c5f7af3c8e116e658a5ba8e3808813ba9bd019e
nataniel.antosik <nataniel.antosik@intive.com>
2023-05-31 09:27:06 +0200

    #64 Adjust branch name regex 1.0.1

--- .github/scripts/branch/checkBranch.sh
+++ .github/scripts/branch/checkBranch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 branch=${GITHUB_HEAD_REF#refs/*/v}
-if ! [[ $branch =~ [a-z]{3}\/[0-9]+-[A-Za-z-]+$ ]]
+if ! [[ $branch =~ [a-z]{3}\/[0-9]+-[^[:cntrl:]]+$ ]]
 then
     echo "::error::Branch is invalid — $branch"
     echo "::error::Branch should looks like - \$acronym/\$issue_number-\$description"
