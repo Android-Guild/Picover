a2e2f0914b83d07eae3b53af21c50c3a48d177cd
Borys Chajdas <b.chajdas@intive.com>
2023-05-31 08:45:49 +0200

    #65 Bump dependencies

--- ktlint-ruleset/build.gradle.kts
+++ ktlint-ruleset/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 dependencies {
-	val version = "0.49.0"
+	val version = "0.49.1"
 	implementation("com.pinterest.ktlint:ktlint-core:$version")
 	implementation("com.pinterest.ktlint:ktlint-cli-ruleset-core:$version")
 }
