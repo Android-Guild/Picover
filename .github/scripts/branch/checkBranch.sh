64ee0a70d5f94d2bc07f5f77894ce8a915ef717f
nataniel.antosik <nataniel.antosik@intive.com>
2023-05-31 09:37:21 +0200

    #64 example 1.0.2 regex

--- .github/scripts/branch/checkBranch.sh
+++ .github/scripts/branch/checkBranch.sh
@@ -4,6 +4,6 @@ branch=${GITHUB_HEAD_REF#refs/*/v}
 if ! [[ $branch =~ [a-z]{3}\/[0-9]+-[^[:cntrl:]]+$ ]]
 then
     echo "::error::Branch is invalid — $branch"
-    echo "::error::Branch should looks like - \$acronym/\$issue_number-\$description"
+    echo "::error::Branch should looks like - \$acronym/\$issue_number-\$description "
     exit 1
 fi
