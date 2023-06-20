cf04ca6414f53e6db2213a9a730a50f58e50b287
nataniel.antosik <nataniel.antosik@intive.com>
2023-06-20 08:07:07 +0200

    #64 Make more readable regex

--- .github/scripts/branch/checkBranch.sh
+++ .github/scripts/branch/checkBranch.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+acronym="[a-z]{3}"
+slash="\/"
+numbers="[0-9]+"
+hyphen="-"
+word="[^[:cntrl:]]+"
+
 branch=${GITHUB_HEAD_REF#refs/*/v}
-if ! [[ $branch =~ [a-z]{3}\/[0-9]+-[^[:cntrl:]]+$ ]]
+if ! [[ $branch =~ $acronym$slash$numbers$hyphen$word ]]
 then
     echo "::error::Branch is invalid — $branch"
     echo "::error::Branch should looks like - \$acronym/\$issue_number-\$description"
--- .github/scripts/commit/checkCommitMessage.sh
+++ .github/scripts/commit/checkCommitMessage.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+hash="^#"
+number="[0-9]+"
+space="[[:space:]]"
+word="[^[:cntrl:]]+"
+
 message=$(git log -1 --pretty=format:"%s" | tr -dc '[:print:]\n')
-if ! [[ "$message" =~ ^#[0-9]+[[:space:]][^[:cntrl:]]+[[:space:]]*$ ]]
+if ! [[ "$message" =~ $hash$number$space$word$space ]]
 then
   echo "::error::Message is invalid — $message"
   echo "::error::Commit message should look like - #\$issue_number \$description"
