b6704cff34e8e060b76c9df31ae605c122cd2665
Marek Macko <marek.macko@intive.com>
2023-09-14 15:13:18 +0200

    #110 Add coroutine dispatcher qualifier

---
+++ app/src/main/kotlin/com/intive/picover/common/coroutines/module/DispatcherModule.kt
@@ -0,0 +1,17 @@
+package com.intive.picover.common.coroutines.module
+
+import com.intive.picover.common.coroutines.dispatcher.Dispatcher
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.Dispatchers
+
+@InstallIn(SingletonComponent::class)
+@Module
+object DispatcherModule {
+
+	@Provides
+	@Dispatcher.IO
+	fun provideIoDispatcher() = Dispatchers.IO
+}
