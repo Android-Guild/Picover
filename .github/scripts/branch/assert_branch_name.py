447ec1479cd2fd8a5cd1dc3363657350cd43c9f8
Marek Macko <marek.macko@intive.com>
2023-09-20 11:11:24 +0200

    #213 Branch template adaptation

--- .github/scripts/branch/assert_branch_name.py
+++ .github/scripts/branch/assert_branch_name.py
@@ -1,7 +1,7 @@
 import re
 import sys
 
-developer_pattern = re.compile(r'[a-z]{3}/[0-9]+-[a-zA-Z0-9-.]+')
+developer_pattern = re.compile(r'[a-z]{3}/[0-9]+/[a-zA-Z0-9-.]+')
 renovate_pattern = re.compile(r'renovate/.+')
 
 
@@ -12,12 +12,13 @@ def assert_branch_name(branch_name):
 
 current_branch_name = sys.argv[1]
 branches = [
-    (True, "rmc/100-configure"),
-    (True, "rmc/200-configure-dependabot"),
-    (True, "rmc/200-bump-kotlin-1.9.0"),
+    (True, "rmc/100/configure"),
+    (True, "rmc/200/configure-dependabot"),
+    (True, "rmc/200/bump-kotlin-1.9.0"),
     (True, "renovate/androidx.lifecycle"),
-    (False, "rm/100-configure"),
-    (False, "rmcx/100-configure"),
+    (False, "rmc/100-configure"),
+    (False, "rm/100/configure"),
+    (False, "rmcx/100/configure"),
     (True, current_branch_name),
 ]
 for is_valid, branch in branches:
