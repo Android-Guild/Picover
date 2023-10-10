78957626d6c8cd4e64823c51ec91aca90ce14aa5
Marek Macko <marek.macko@intive.com>
2023-10-10 14:01:00 +0200

    #189 Inline theme in SignInIntent configuration

--- app/src/test/kotlin/com/intive/picover/auth/intent/builder/SignInIntentBuilderTest.kt
+++ app/src/test/kotlin/com/intive/picover/auth/intent/builder/SignInIntentBuilderTest.kt
@@ -3,14 +3,12 @@ package com.intive.picover.auth.intent.builder
 import android.content.Intent
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.AuthUI.IdpConfig.EmailBuilder
-import com.intive.picover.R
 import com.intive.picover.auth.intent.SignInIntent
 import io.kotest.core.spec.style.ShouldSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.mockkConstructor
-import com.firebase.ui.auth.AuthUI.SignInIntentBuilder as FirebaseSignInIntentBuilder
 
 class SignInIntentBuilderTest : ShouldSpec(
 	{
@@ -21,13 +19,12 @@ class SignInIntentBuilderTest : ShouldSpec(
 		should("return intent wrapped by SignInIntent WHEN build called") {
 			val providers: AuthUI.IdpConfig = mockk()
 			val signInIntent: Intent = mockk()
-			val firebaseSignInIntentBuilder: FirebaseSignInIntentBuilder = mockk {
-				every { setTheme(R.style.Theme_Picover) } returns this
-				every { setAvailableProviders(listOf(providers)) } returns this
-				every { build() } returns signInIntent
-			}
 			val authUi: AuthUI = mockk {
-				every { createSignInIntentBuilder() } returns firebaseSignInIntentBuilder
+				every { createSignInIntentBuilder() } returns mockk {
+					every { setTheme(any()) } returns this
+					every { setAvailableProviders(listOf(providers)) } returns this
+					every { build() } returns signInIntent
+				}
 			}
 			every { anyConstructed<EmailBuilder>().build() } returns providers
 
