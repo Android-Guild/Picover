345ea858fc3e6c885111b7259a3c9ff8612f83c8
Marek Macko <marek.macko@intive.com>
2023-10-19 12:48:47 +0200

    #120 Photo uploading notification

--- app/src/test/kotlin/com/intive/picover/photos/work/UploadPhotoWorkerTest.kt
+++ app/src/test/kotlin/com/intive/picover/photos/work/UploadPhotoWorkerTest.kt
@@ -1,23 +1,31 @@
 package com.intive.picover.photos.work
 
+import android.app.NotificationManager
+import android.content.Context
 import android.net.Uri
 import androidx.work.ListenableWorker
 import androidx.work.WorkerParameters
 import androidx.work.workDataOf
 import com.google.firebase.storage.StorageReference
+import com.intive.picover.common.notification.provider.PicoverNotificationProvider
 import io.kotest.core.spec.style.ShouldSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.coEvery
 import io.mockk.every
+import io.mockk.just
 import io.mockk.mockk
 import io.mockk.mockkStatic
+import io.mockk.runs
+import io.mockk.spyk
 import io.mockk.unmockkAll
 
 class UploadPhotoWorkerTest : ShouldSpec(
 	{
+		val context: Context = mockk(relaxed = true)
 		val workerParams: WorkerParameters = mockk(relaxed = true)
 		val storageReference: StorageReference = mockk()
-		val tested = UploadPhotoWorker(mockk(), workerParams, storageReference)
+		val notificationProvider: PicoverNotificationProvider = mockk(relaxed = true)
+		val tested = spyk(UploadPhotoWorker(context, workerParams, storageReference, notificationProvider))
 
 		beforeSpec {
 			mockkStatic(Uri::parse)
@@ -28,8 +36,11 @@ class UploadPhotoWorkerTest : ShouldSpec(
 		}
 
 		should("return success WHEN photo uri uploaded successfully") {
+			// TODO migrate to https://developer.android.com/guide/background/testing/persistent/integration-testing
 			val photoUri: Uri = mockk()
+			every { context.getSystemService(Context.NOTIFICATION_SERVICE) } returns mockk<NotificationManager>(relaxed = true)
 			every { workerParams.inputData } returns workDataOf("PHOTO_URI" to "photo1.jpg")
+			coEvery { tested.setForeground(any()) } just runs
 			every { Uri.parse("photo1.jpg") } returns photoUri
 			coEvery { storageReference.child("image").child(any()).putFile(photoUri) } returns mockk {
 				every { isComplete } returns true
