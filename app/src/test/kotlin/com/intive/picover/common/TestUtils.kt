4832f3fedc905f0c32920f1f191b66fd361819e0
Marek Macko <marek.macko@intive.com>
2023-10-19 10:57:43 +0200

    #329 Enable function naming rule

--- app/src/test/kotlin/com/intive/picover/common/TestUtils.kt
+++ app/src/test/kotlin/com/intive/picover/common/TestUtils.kt
@@ -3,6 +3,7 @@ package com.intive.picover.common
 import io.mockk.MockKStubScope
 
 fun <T> mockkAnswer(block: MockKStubScope<T, T>.() -> Unit) =
+	@Suppress("ktlint:standard:function-naming")
 	fun MockKStubScope<T, T>.() {
 		block()
 	}
