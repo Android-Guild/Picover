2936d708c2244197857f98de6b9a5efef1232acf
Marek Macko <marek.macko@intive.com>
2023-10-19 12:48:47 +0200

    #120 Photos staggered grid

--- app/src/main/kotlin/com/intive/picover/images/view/ImagesScreen.kt
+++ app/src/main/kotlin/com/intive/picover/images/view/ImagesScreen.kt
@@ -2,71 +2,58 @@ package com.intive.picover.images.view
 
 import android.net.Uri
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.intive.picover.common.error.PicoverGenericError
 import com.intive.picover.common.loader.PicoverLoader
-import com.intive.picover.common.viewmodel.state.ViewModelState
 import com.intive.picover.images.viewmodel.ImagesViewModel
 import com.skydoves.landscapist.coil.CoilImage
 
 @Composable
 fun ImagesScreen(viewModel: ImagesViewModel) {
 	val state by viewModel.state
-	ValidateFirebaseStorageState(state)
-}
-
-@Composable
-private fun ValidateFirebaseStorageState(state: ViewModelState<List<Uri>>) {
 	when {
-		state.isLoaded() -> LazyColumn(
-			content = {
-				items(state.data()) {
-					ListItem(uri = it)
-				}
-			},
-			horizontalAlignment = Alignment.CenterHorizontally,
-			verticalArrangement = Arrangement.Center,
-		)
-
-		state.isError() -> {
-			// TODO-KMA Handle the error
-		}
-
+		state.isLoaded() -> PhotosGrid(state.data())
+		state.isError() -> PicoverGenericError()
 		state.isLoading() -> PicoverLoader(modifier = Modifier.fillMaxSize())
 	}
 }
 
 @Composable
-private fun ListItem(uri: Uri) {
-	Card(
-		modifier = Modifier
-			.padding(8.dp)
-			.height(250.dp),
-		colors = CardDefaults.cardColors(containerColor = Color.White),
-		shape = RoundedCornerShape(15.dp),
-		elevation = CardDefaults.elevatedCardElevation(defaultElevation = 4.dp),
-	) {
-		CoilImage(
-			modifier = Modifier.fillMaxWidth(),
-			imageModel = { uri },
-			loading = {
-				PicoverLoader(
-					modifier = Modifier.fillMaxSize(),
-				)
-			},
-		)
+private fun PhotosGrid(uris: List<Uri>) {
+	Box(Modifier.fillMaxSize()) {
+		LazyVerticalStaggeredGrid(
+			columns = StaggeredGridCells.Adaptive(minSize = 120.dp),
+			verticalItemSpacing = 4.dp,
+			horizontalArrangement = Arrangement.spacedBy(4.dp),
+		) {
+			items(uris) {
+				Photo(uri = it)
+			}
+		}
 	}
+}
+
+@Composable
+private fun Photo(uri: Uri) {
+	CoilImage(
+		modifier = Modifier
+			.defaultMinSize(minHeight = 120.dp)
+			.wrapContentHeight(),
+		imageModel = { uri },
+		loading = {
+			PicoverLoader(
+				modifier = Modifier.fillMaxSize(),
+			)
+		},
+	)
 }
