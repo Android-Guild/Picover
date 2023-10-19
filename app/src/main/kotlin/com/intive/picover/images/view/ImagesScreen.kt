154e30fbabc1c8cfa7dffbc5b906162886a72be7
Marek Macko <marek.macko@intive.com>
2023-10-19 12:48:47 +0200

    #120 Upload new photo use case stub

--- app/src/main/kotlin/com/intive/picover/images/view/ImagesScreen.kt
+++ app/src/main/kotlin/com/intive/picover/images/view/ImagesScreen.kt
@@ -1,35 +1,50 @@
 package com.intive.picover.images.view
 
 import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PhotoCamera
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.intive.picover.common.error.PicoverGenericError
 import com.intive.picover.common.loader.PicoverLoader
+import com.intive.picover.common.result.TakePictureContract
 import com.intive.picover.images.viewmodel.ImagesViewModel
 import com.skydoves.landscapist.coil.CoilImage
 
 @Composable
 fun ImagesScreen(viewModel: ImagesViewModel) {
+	val takePictureResultLauncher = rememberLauncherForActivityResult(TakePictureContract()) {
+		it?.let(viewModel::uploadPhoto)
+	}
 	val state by viewModel.state
 	when {
-		state.isLoaded() -> PhotosGrid(state.data())
+		state.isLoaded() -> PhotosGrid(state.data()) {
+			takePictureResultLauncher.launch(Unit)
+		}
+
 		state.isError() -> PicoverGenericError()
 		state.isLoading() -> PicoverLoader(modifier = Modifier.fillMaxSize())
 	}
 }
 
 @Composable
-private fun PhotosGrid(uris: List<Uri>) {
+private fun PhotosGrid(uris: List<Uri>, onTakeImage: () -> Unit) {
 	Box(Modifier.fillMaxSize()) {
 		LazyVerticalStaggeredGrid(
 			columns = StaggeredGridCells.Adaptive(minSize = 120.dp),
@@ -39,6 +54,16 @@ private fun PhotosGrid(uris: List<Uri>) {
 			items(uris) {
 				Photo(uri = it)
 			}
+		}
+		FloatingActionButton(
+			modifier = Modifier
+				.padding(12.dp)
+				.align(Alignment.BottomEnd),
+			onClick = onTakeImage,
+			containerColor = MaterialTheme.colorScheme.secondaryContainer,
+			contentColor = MaterialTheme.colorScheme.secondary,
+		) {
+			Icon(Icons.Filled.PhotoCamera, null)
 		}
 	}
 }
--- app/src/main/kotlin/com/intive/picover/images/viewmodel/ImagesViewModel.kt
+++ app/src/main/kotlin/com/intive/picover/images/viewmodel/ImagesViewModel.kt
@@ -6,6 +6,7 @@ import com.intive.picover.common.viewmodel.StatefulViewModel
 import com.intive.picover.common.viewmodel.state.ViewModelState.Error
 import com.intive.picover.common.viewmodel.state.ViewModelState.Loaded
 import com.intive.picover.images.repository.ImagesRepository
+import com.intive.picover.photos.usecase.UploadPhotoUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import kotlinx.coroutines.launch
@@ -13,12 +14,17 @@ import kotlinx.coroutines.launch
 @HiltViewModel
 class ImagesViewModel @Inject constructor(
 	private val repository: ImagesRepository,
+	private val uploadPhotoUseCase: UploadPhotoUseCase,
 ) : StatefulViewModel<List<Uri>>() {
 
 	init {
 		viewModelScope.launch {
 			loadImages()
 		}
+	}
+
+	fun uploadPhoto(uri: Uri) {
+		uploadPhotoUseCase(uri)
 	}
 
 	private suspend fun loadImages() {
